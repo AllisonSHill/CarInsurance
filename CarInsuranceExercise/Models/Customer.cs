f83575e0e84c611b22d3de07119d92b9d9f7dfc2
allisonhill00 <allison.m.s.hill@gmail.com>
2019-09-30 11:43:02 -0600

    add everything

---
+++ CarInsuranceExercise/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsuranceExercise.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string carYear { get; set; }
+        public string carMake { get; set; }
+        public string carModel { get; set; }
+        public string duiCheck { get; set; }
+        public int speedingTickets { get; set; }
+        public string coverageType { get; set; }
+        public decimal quote { get; set; }
+    }
+}
